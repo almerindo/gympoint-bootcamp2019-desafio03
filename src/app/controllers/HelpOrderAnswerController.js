d7450d4af9acd8b0f8af2c38323833afd4dbe7f3
Almerindo Rehem <almerindo@almerindo-tux>
2019-11-11 15:46:34 -0300

    freat: Concluido helporders - a questao 4 do desafio 03
     Changes to be committed:
    	modified:   src/app/controllers/HelpOrderAnswerController.js
    	modified:   src/app/controllers/HelpOrderController.js

--- src/app/controllers/HelpOrderAnswerController.js
+++ src/app/controllers/HelpOrderAnswerController.js
@@ -1,6 +1,35 @@
+import * as Yup from 'yup';
+
+import HelpOrder from '../models/HelpOrder';
+import Student from '../models/Student';
+
 class HelpOrderAnswerController {
   async store(req, res) {
-    return res.json();
+    const { id } = req.params;
+
+    // valida se o ID está presente
+    const schema = Yup.object().shape({
+      id: Yup.number().required(),
+    });
+
+    if (!(await schema.isValid(req.params))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+    // Verifica se answer está presente
+    const { answer } = req.body;
+    if (!answer) {
+      return res.status(400).json(`Answer must be informed`);
+    }
+
+    const helpOrder = await HelpOrder.findByPk(id);
+    if (!helpOrder) {
+      return res.status(400).json(`HelpOrder ID: ${id} does not exists`);
+    }
+
+    helpOrder.answer = answer;
+    helpOrder.updated_at = new Date();
+
+    return res.json(await helpOrder.save());
   }
 }
 export default new HelpOrderAnswerController();
--- src/app/controllers/HelpOrderController.js
+++ src/app/controllers/HelpOrderController.js
@@ -1,10 +1,36 @@
+import * as Yup from 'yup';
+
+import HelpOrder from '../models/HelpOrder';
+import Student from '../models/Student';
+
 class HelpOrderController {
   async show(req, res) {
-    return res.json();
+    const helporders = await HelpOrder.findAll();
+    return res.json(helporders);
   }
 
   async store(req, res) {
-    return res.json();
+    const { id } = req.params;
+
+    const schema = Yup.object().shape({
+      id: Yup.number().required(),
+    });
+
+    if (!(await schema.isValid(req.params))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    const student = await Student.findByPk(id);
+    if (!student) {
+      return res.status(400).json(`Student ID: ${id} does not exists`);
+    }
+
+    const { question } = req.body;
+    if (!question) {
+      return res.status(400).json(`Question must be informed`);
+    }
+
+    return res.json(await HelpOrder.create({ student_id: id, question }));
   }
 }
 export default new HelpOrderController();
